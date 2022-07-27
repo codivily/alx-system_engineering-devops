0cec31174140623bf21a98768f78a4c74b07c350
root <root@u-station>
2022-07-27 00:00:37 +0000

    requirement update

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,7 +1,4 @@
 #!/usr/bin/env ruby
-# This script match the a string argument to a regular expression.
+puts ARGV[0].scan(/School/).join
 
-if ARGV.length > 0
-  puts ARGV[0].scan(/School/).join("")
-end
 
