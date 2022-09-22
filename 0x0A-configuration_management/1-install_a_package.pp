bb41f58d0ee05a40837e7b769e8bc22990e78b6f
codivily <codivily@gmail.com>
2022-09-22 21:30:31 +0000

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,5 @@
 # install flask using pip3
-class flask {
-  package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
-  }
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
-include flask
