61f502f1b71a2be69eac5df506c02204279ec9b8
codivily <codivily@gmail.com>
2022-10-20 20:51:42 +0000

    0-gather_data_from_an_API.py created

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Returns information about employee TODO list progress"""
+import requests
+import sys
+
+
+def users(id):
+    r = requests.get('https://jsonplaceholder.typicode.com/users/' + id)
+    return r.json()
+
+
+def todos(userId, done=None):
+    url = 'https://jsonplaceholder.typicode.com/users/' + userId + '/todos'
+
+    if done is not None:
+        if done:
+            url += '?completed=true'
+        else:
+            url += '?completed=false'
+    r = requests.get(url)
+    return r.json()
+
+
+if __name__ == '__main__':
+    userId = sys.argv[1]
+
+    user = users(userId)
+    todos = todos(userId)
+    completed_todos = [todo for todo in todos if todo['completed']]
+
+    print('Employee {} is done with tasks({}/{}):'.format(
+        user['name'],
+        len(completed_todos),
+        len(todos)))
+
+    for todo in completed_todos:
+        print('\t' + todo['title'])
