2fb02bd4b3bbaa2841ab8be301f595bd42730953
codivily <codivily@gmail.com>
2023-05-09 08:01:43 +0000

    0-subs.py fix

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A function that queries the Reddit API and returns the number of subscribers
+for a given subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
+    res = requests.get(url, headers={'User-Agent': "0x16-api_advanced:alx"})
+    if res.status_code != requests.codes.ok:
+        return 0
+    return res.json()['data']['subscribers']
+
+
+if __name__ == '__main__':
+    n = number_of_subscribers('python')
+    print(n)
