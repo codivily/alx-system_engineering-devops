7cdcb226299ae03b64209368d86b758cb2f79902
codivily <codivily@gmail.com>
2024-08-16 23:03:44 +0000

    update: 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -14,9 +14,12 @@ def top_ten(subreddit):
     headers = {'User-Agent': '0x16-api_advanced:alx'}
     res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code == requests.codes.ok:
-        data = res.json()['data']
-        hot_posts = data['children']
-        for post in hot_posts:
-            print(post['data']['title'])
+        try:
+            data = res.json()['data']
+            hot_posts = data['children']
+            for post in hot_posts:
+                print(post['data']['title'])
+        except Error:
+            print(None)
     else:
         print(None)
