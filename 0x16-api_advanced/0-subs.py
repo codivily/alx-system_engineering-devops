d5d5501962a96a5035e57412b2ce1e391ce15dd1
codivily <90767560+CryptoAfrique@users.noreply.github.com>
2024-08-16 00:19:31 +0000

    add: 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A function that queries the Reddit API and returns the number of subscribers
+for a given subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
+    res = requests.get(url, headers={'User-Agent': "0x16-api_advanced:alx"})
+    if res.status_code == requests.codes.ok:
+        data = res.json()['data']
+        if 'subscribers' not in data:
+            return 0
+        return data['subscribers']
+    return 0
